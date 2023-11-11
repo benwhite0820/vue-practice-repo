24c6e15391502d00b4557a0df1e60df9d48a0b3b
benwhite0820 <dragon7230@gmail.com>
2023-11-11 21:34:09 +0800

    two way data binding

---
+++ src/components/ReviewForm.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { reactive } from 'vue';
+
+const review = reactive({ name: '', content: '', rating: null });
+</script>
+
+<template>
+  <form class="review-form">
+    <h3>Leave a review</h3>
+    <label for="name">Name:</label>
+    <input id="name" v-model="review.name" />
+
+    <label for="review">Review:</label>
+    <textarea id="review" v-model="review.content"></textarea>
+
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="review.rating">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </form>
+</template>
+
+<label for="rating">Rating:</label>
+<select id="rating" v-model.number="review.rating">
+  <option>1</option>
+  <option>2</option>
+  <option>3</option>
+  <option>4</option>
+  <option>5</option>
+</select>
