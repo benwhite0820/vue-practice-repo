263bd77c492350fb7cd375f7ad3e80f5436db784
benwhite0820 <dragon7230@gmail.com>
2023-11-11 20:34:09 +0800

    props

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,13 @@
 <script setup>
 import { ref } from 'vue';
-import socksGreenImage from './assets/socks_green.jpeg';
-import socksBlueImage from './assets/socks_blue.jpeg';
-
-const product = ref('Socks');
-const image = ref(socksGreenImage);
-const inStock = ref(false);
-
-const details = ref(['50% cotton', '30% wool', '20% polyester']);
-
-const variants = ref([
-  { id: 2234, color: 'green', image: socksGreenImage },
-  { id: 2235, color: 'blue', image: socksBlueImage },
-]);
+import ProductDisplay from '@/components/ProductDisplay.vue';
 
 const cart = ref(0);
-
-const addToCart = () => cart.value++;
-
-const updateImage = (hoverImage) => (image.value = hoverImage);
+const premium = ref(true);
 </script>
 
 <template>
   <div class="nav-bar"></div>
   <div class="cart">Cart{{ cart }}</div>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">
-        <img :src="image" alt="socks" />
-      </div>
-      <div class="product-info">
-        <h1>{{ product }}</h1>
-        <p v-if="inStock">In Stock</p>
-        <p v-else>Out of Stock</p>
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-
-        <div
-          v-for="{ id, color, image } in variants"
-          :key="id"
-          @mouseover="() => updateImage(image)"
-          class="color-circle"
-          :style="{ backgroundColor: color }"
-        ></div>
-      </div>
-      <button
-        class="button"
-        :class="{ disabledButton: !inStock }"
-        @click="addToCart"
-        :disabled="!inStock"
-      >
-        Add to Cart
-      </button>
-    </div>
-  </div>
+  <ProductDisplay :premium="premium"></ProductDisplay>
 </template>
---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,74 @@
+<script setup>
+import { ref } from 'vue';
+import socksGreenImage from '@/assets/socks_green.jpeg';
+import socksBlueImage from '@/assets/socks_blue.jpeg';
+import { computed } from '@vue/reactivity';
+
+const { premium } = defineProps({
+  premium: Boolean,
+});
+
+const product = ref('Socks');
+const brand = ref('Vue Mastery');
+
+const selectedVariant = ref(0);
+
+const details = ref(['50% cotton', '30% wool', '20% polyester']);
+
+const variants = ref([
+  { id: 2234, color: 'green', image: socksGreenImage, quantity: 50 },
+  { id: 2235, color: 'blue', image: socksBlueImage, quantity: 0 },
+]);
+
+const title = computed(() => `${brand.value} ${product.value}`);
+
+const image = computed(() => variants.value[selectedVariant.value].image);
+
+const inStock = computed(
+  () => variants.value[selectedVariant.value].quantity > 0
+);
+
+const shipping = computed(() => (premium ? 'free' : 2.99));
+
+const addToCart = () => cart.value++;
+
+const updateVariant = (index) => {
+  selectedVariant.value = index;
+};
+</script>
+
+<template>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img :src="image" alt="socks" />
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+        <p>Shipping: {{ shipping }}</p>
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+
+        <div
+          v-for="({ id, color }, index) in variants"
+          :key="id"
+          @mouseover="() => updateVariant(index)"
+          class="color-circle"
+          :style="{ backgroundColor: color }"
+        ></div>
+
+        <button
+          class="button"
+          :class="{ disabledButton: !inStock }"
+          @click="addToCart"
+          :disabled="!inStock"
+        >
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
