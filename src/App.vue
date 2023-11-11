275c5456bd2ff93d2c2f418ea299542e8ee22d14
benwhite0820 <dragon7230@gmail.com>
2023-11-11 17:32:43 +0800

    basic vue template and syntax

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,58 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+import socksGreenImage from './assets/socks_green.jpeg';
+import socksBlueImage from './assets/socks_blue.jpeg';
+
+const product = ref('Socks');
+const image = ref(socksGreenImage);
+const inStock = ref(false);
+
+const details = ref(['50% cotton', '30% wool', '20% polyester']);
+
+const variants = ref([
+  { id: 2234, color: 'green', image: socksGreenImage },
+  { id: 2235, color: 'blue', image: socksBlueImage },
+]);
+
+const cart = ref(0);
+
+const addToCart = () => cart.value++;
+
+const updateImage = (hoverImage) => (image.value = hoverImage);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="nav-bar"></div>
+  <div class="cart">Cart{{ cart }}</div>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img :src="image" alt="socks" />
+      </div>
+      <div class="product-info">
+        <h1>{{ product }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <div
+          v-for="{ id, color, image } in variants"
+          :key="id"
+          @mouseover="() => updateImage(image)"
+          class="color-circle"
+          :style="{ backgroundColor: color }"
+        ></div>
+      </div>
+      <button
+        class="button"
+        :class="{ disabledButton: !inStock }"
+        @click="addToCart"
+        :disabled="!inStock"
+      >
+        Add to Cart
+      </button>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
